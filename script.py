0c2ae546fa195c430bb2cb4cddc6c58b5d4a44cc
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2024-10-23 09:31:25 +0200

    :fire: STEP 2 :fire: and add review_rating

--- script.py
+++ script.py
@@ -2,10 +2,53 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-table_rows = []
+url = 'https://books.toscrape.com/'
+data_pages = []
+
+def scrap_one_page(page_url) :
+    response = requests.get(url + page_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table_rows = soup.select('tr > td')
+
+    product_page_url = url + page_url
+    universal_product_code = table_rows[0].text
+    title = soup.find('h1').text
+    price_including_tax = table_rows[2].text.replace('Â', '')
+    price_excluding_tax = table_rows[3].text.replace('Â', '')
+    number_available = table_rows[5].text.split()[2].replace('(', '')
+    product_description = soup.find_all('p')[3].text
+    category = soup.select('li > a')[2].text
+    review_rating = soup.find(class_='star-rating').get('class')[1]
+    image_url = url.replace('catalogue/a-light-in-the-attic_1000/index.html', '') + soup.find('img')['src'].replace('../../', '')
+
+    return  [
+        product_page_url, 
+        universal_product_code,
+        title,
+        price_including_tax,
+        price_excluding_tax,
+        number_available,
+        product_description,
+        category,
+        review_rating,
+        image_url
+        ]
+
+def scrap_category(category_url) :
+    print(url + category_url)
+    response = requests.get(url + category_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    links = soup.select('div.image_container > a')
+    for link in links:
+        data_page = scrap_one_page('catalogue/' + link['href'].replace('../../../', ''))
+        data_pages.append(data_page)
+
+    next_button = soup.find(class_='next')
+    if next_button != None :
+        next_url = next_button.find('a').get('href')
+        scrap_category(f'catalogue/category/books/default_15/{next_url}')
+
 
 with open('book.csv', 'w', newline='', encoding='utf-8') as fichier_csv:
     writter = csv.writer(fichier_csv)
@@ -21,30 +64,11 @@ with open('book.csv', 'w', newline='', encoding='utf-8') as fichier_csv:
         'review_rating',
         'image_url'
         ])
-    table_rows = soup.select('tr > td')
-
-    product_page_url = url
-    universal_product_code = table_rows[0].text
-    title = soup.find('h1').text
-    price_including_tax = table_rows[2].text.replace('Â', '')
-    price_excluding_tax = table_rows[3].text.replace('Â', '')
-    number_available = table_rows[5].text.split()[2].replace('(', '')
-    product_description = soup.find_all('p')[3].text
-    category = soup.select('li > a')[2].text
-    review_rating = 'n/a'
-    image_url = url.replace('catalogue/a-light-in-the-attic_1000/index.html', '') + soup.find('img')['src'].replace('../../', '')
-
-    writter.writerow(
-        [
-        product_page_url, 
-        universal_product_code,
-        title,
-        price_including_tax,
-        price_excluding_tax,
-        number_available,
-        product_description,
-        category,
-        review_rating,
-        image_url
-        ]
-    )
+    
+    scrap_category('catalogue/category/books/default_15/index.html')
+    # print(data_pages)
+    
+    for data in data_pages :
+        writter.writerow(
+            data
+        )
